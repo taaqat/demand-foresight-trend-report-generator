452330b42077825f691e00b0218b4c873e14af6d
taaqat <taaqat@gmail.com>
2024-12-20 15:06:07 +0800

    Fixed theme picture loading part

--- index.py
+++ index.py
@@ -10,43 +10,48 @@ from streamlit_authenticator import Hasher
 from managers.session_manager import SessionManager
 from managers.data_manager import DataManager
 
+
+# *********** Config ***********
+# * page layout
 if "page_config_set" not in st.session_state:
     st.set_page_config(page_title='Demand Foresight Tools', page_icon=":tada:", layout="wide")
     st.session_state["page_config_set"] = True
 
-
-# *********** Config ***********
+# * user authentication data
 if "config" not in st.session_state:
     with open("users.yaml") as file:
         st.session_state.config = yaml.load(file, Loader=SafeLoader)
 
+# * configure the start-date, end-date, and theme picture path for year month in 2024
+#  {'2024 MONTH': [START_DATE, END_DATE, PIC_PATH]}
 st.session_state["ym_mapping"] = {
-    '2024 January': ["2024-01-01", "2024-01-31"],
-    '2024 February': ["2024-02-01", "2024-02-29"],
-    '2024 March': ["2024-03-01", "2024-03-31"],
-    '2024 April': ["2024-04-01", "2024-04-30"],
-    '2024 May': ["2024-05-01", "2024-05-31"],
-    '2024 June': ["2024-06-01", "2024-06-30"],
-    '2024 July': ["2024-07-01", "2024-07-31"],
-    '2024 August': ["2024-08-01", "2024-08-31"],
-    '2024 September': ["2024-09-01", "2024-10-01"],
-    '2024 October': ["2024-10-01", "2024-10-31"],
-    '2024 November': ["2024-11-01", "2024-11-27"]
+    '2024 January': ["2024-01-01", "2024-01-31", 1],
+    '2024 February': ["2024-02-01", "2024-02-29", 2],
+    '2024 March': ["2024-03-01", "2024-03-31", 3],
+    '2024 April': ["2024-04-01", "2024-04-30", 1],
+    '2024 May': ["2024-05-01", "2024-05-31", 2],
+    '2024 June': ["2024-06-01", "2024-06-30", 3],
+    '2024 July': ["2024-07-01", "2024-07-31", 1],
+    '2024 August': ["2024-08-01", "2024-08-31", 2],
+    '2024 September': ["2024-09-01", "2024-10-01", 3],
+    '2024 October': ["2024-10-01", "2024-10-31", 1],
+    '2024 November': ["2024-11-01", "2024-11-27", 2]
  }
 
 
 
 
-# *** entry point after login ***
+# *** function that would be called after login (entry point of the app) ***
 def main():
 
     st.title("WELCOME")
     
 
-    # button style setting
+    # ---------------------------------------------------
+    # *** button style setting
     st.markdown("""<style>
     div.stButton > button {
-        width: 100%;  /* 設置按鈕寬度為頁面寬度的 50% */
+        width: 100%;  
         height: 50px;
         margin-left: 0;
         margin-right: auto;
@@ -58,7 +63,8 @@ def main():
     </style>
     """, unsafe_allow_html=True)
 
-    # *** navigation bar with st.page_link()
+    # ---------------------------------------------------
+    # *** navigation bar by st.page_link()
     st.session_state['logged_in'] = True
 
     st.sidebar.header("資策會 Demand Foresight Tools")
@@ -79,23 +85,40 @@ def main():
     st.sidebar.write("**視覺化界面**")
     # st.sidebar.page_link("[小賴做的視覺化界面]", label = "", icon = ':material/add_circle:')
     
-    # *** load download UI
+    # ----------------------------------------------------------------------------
+    # *** STEEP Report GALLERY
 
-    @st.cache_data
-    def load_steep_download_pics(ym):
+    # * User choose the year month *
+    ym = st.selectbox("Choose a month to download the STEEP reports", st.session_state['ym_mapping'].keys())
+
+    # * Function for loading STEEP +B report dynamically *
+    @st.cache_data                          # * cache the data to reduce data transmission burden
+    def load_steep_download_pics(ym, pic_id):
+
+        # * define six blocks (STEEP +B respectively)
         cols = [col for group in (st.columns(3), st.columns(3))for col in group]
-        for i, (col, topic) in enumerate(zip(cols, ["social", "technological", "economic", "environmental", "political", "business_and_investment"])):
+
+        # * for each topic, get the powerpoint from III database, and the theme picture in /pic folder, then render it like a gallery
+        for col, topic in zip(cols, ["social", "technological", "economic", "environmental", "political", "business_and_investment"]):
             with col:
                 try:
+                    # * fetch pptx based on start_date, end_date for corresponding month
                     link_html_obj = DataManager.get_output_download_link(st.session_state["ym_mapping"][ym][0],
                                                                         st.session_state["ym_mapping"][ym][1], 
                                                                         topic, 
                                                                         'pptx', 
                                                                         'steep')
+                    
+                    # * extract pptx base64 string
                     pptx_base64 = re.search('href = "(.+)" download', link_html_obj).group(1)
-                    image_base64 = DataManager.image_to_b64(f"./pics/{topic}/{i % 3 + 1}.png")
+
+                    # * encode theme picture into base64 string
+                    image_base64 = DataManager.image_to_b64(f"./pics/{topic}/{pic_id}.png")
+
+                    # * specify the filename (user will see this filename when they download)
                     file_name = f"{ym}-{topic}"
 
+                    # * create download link by HTML structure & CSS setting
                     download_link = f"""
                     <style>
                     .hover-container {{
@@ -136,22 +159,21 @@ def main():
                         <div class="hover-message">Click to download</div>
                     </div>
                     """
+
+                    # * display it with st.markdown()
                     st.markdown(download_link, unsafe_allow_html = True)
-                except:
-                    # st.write("No such file")
+                except Exception as e:
+                    # st.write(e)
                     pass
     
-    ym = st.selectbox("Choose a month to download the STEEP reports", st.session_state['ym_mapping'].keys())
-    load_steep_download_pics(ym)
+    # * ym = ym: the year-month requested by users
+    # * pic_id = st.session_state['ym_mapping'][ym][2]: 為了要讓每個月的照片不同，動態讀取 session_state 中的 pic path（本來用 randint 但這方法會讓 st.cache_data 失效）
+    load_steep_download_pics(ym = ym, pic_id = st.session_state['ym_mapping'][ym][2])
 
     
 
-    
-
-# *** 目前因為我改了 navigation bar 的設定（是用自己設定的 page_link 設定），當 app 開啟後連入此 py 檔，會立刻導向 page_demo.py！
-# *** Authentication 邏輯因此遇到設計上的問題，先不處理
-    
-# *** Authentication ***
+# ------------------------------------------------------------------------------------------------------
+# *** Authentication & Call the main function ***
 
 authenticator = stauth.Authenticate(
     st.session_state.config['credentials'],

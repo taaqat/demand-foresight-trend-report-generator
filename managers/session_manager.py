c64f5f3086e38afde2092ce10a9ae48a8c0c646f
Huang Lin Chun <huang0jin@gmail.com>
2025-02-10 18:03:52 +0800

    Added github branch link

--- managers/session_manager.py
+++ managers/session_manager.py
@@ -177,7 +177,7 @@ class SessionManager:
         response = requests.get("https://api.ipify.org?format=json")
         public_ip = response.json()["ip"]
 
-        st.caption(f"Deployed IP Address: **:blue[{public_ip}]**")
+        st.caption(f"Deployed IP: **:blue[{public_ip}]**  |  Branch [**:orange[main]**](https://github.com/taaqat/demand-foresight-trend-report-generator)")
 
     @staticmethod
     @st.dialog("Session States", width = 'large')
